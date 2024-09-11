63a4fd9bbbb07db4115b9c4d859b3dfa0eaf04cf
Danila <nazaraenko_danila@inbox.ru>
2024-09-11 19:50:21 +0300

    new commit

--- event.js
+++ event.js
@@ -1,14 +1,29 @@
 window.onload = init;
-function init() {
+/**function init() {
     var image = document.getElementsByTagName("img")
     for (let i = 0; i < image.length; i++) {
         image[i].onclick=showAnswer
     }
     
+}*/
+function init() {
+    var image = document.getElementsByTagName("img")
+    for (let i = 0; i < image.length; i++) {
+        image[i].onmouseover=showAnswer
+    }
+    
 }
+
 function showAnswer(eventObj) {
     var image =eventObj.target;
     var name = image.id;
     name = name + ".jpg";
     image.src = "image/" + name; 
+    setTimeout(reblur, 2000, image);
+}
+6
+function reblur(image) {
+    var name = image.id;
+    name = name +"blur.jpg";
+    image.src = "image/" + name;
 }
